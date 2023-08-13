434022d080dd4b7b92db2ba03c7bc9e0217abb2d
bintangmesir <bintangmesir@gmail.com>
2023-08-14 01:01:20 +0700

    add folder variable dan tipe data

---
+++ tipe data/main.py
@@ -0,0 +1,31 @@
+x = 10
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah int
+
+x = 3.14
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah float
+
+x = "Hello world"
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah str
+
+x = True
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah bool
+
+x = [1,2,3]
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah list
+
+x = ("a","b","c")
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah tuple
+
+x = {1,2,3}
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah set
+
+x = {"name": "jhon", "age": "20"}
+print('Tipe data x adalah',type(x).__name__)
+# Tipe data x adalah dict
---
+++ variable/main.py
@@ -0,0 +1,11 @@
+def main():
+    # Mendefinisikan dan menginisialisasi variabel
+    bahasa_pemrograman = "python"
+    versi = "3.11.4"
+
+    # Mencetak output pada terminal
+    print(f"{bahasa_pemrograman} sekarang sudah versi {versi}")
+
+
+if __name__ == "__main__":
+    main()
