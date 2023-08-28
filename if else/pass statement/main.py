4b8e3236e9e55959c64842ae5640ecbbe6f0e585
bintangmesir <bintangmesir@gmail.com>
2023-08-28 22:41:16 +0700

    add if else folder

---
+++ if else/pass statement/main.py
@@ -0,0 +1,14 @@
+def main():
+  # Deklarasi variable untuk pengkondisian
+  a = 33
+  b = 200
+  
+  # Pernyataan "if" tidak boleh kosong, 
+  # Jika dengan alasan tertentu memiliki if tanpa konten
+  # bisa masukkan pernyataan "pass" 
+  # untuk menghindari munculnya kesalahan.
+  if b > a:
+    pass
+  
+if __name__ == "__main__":
+  main()
