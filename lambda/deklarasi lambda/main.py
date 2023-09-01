930f6647cc8e083422d378bdee5f0fdaf95bf5e5
bintangmesir <bintangmesir@gmail.com>
2023-09-01 13:05:43 +0700

    add lambda folder

---
+++ lambda/deklarasi lambda/main.py
@@ -0,0 +1,9 @@
+def main():
+  # Mendefinisikan fungsi lambda yang menggandakan angka
+  double = lambda x: x * 2
+  # Memanggil fungsi lambda
+  result = double(5)  
+  print(result) 
+  
+if __name__ == '__main__':
+  main()
